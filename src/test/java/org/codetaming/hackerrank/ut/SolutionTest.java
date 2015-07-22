b5109f4f67edae952a9963996225896db4aea229
Daniel Vaughan <dan@danielvaughan.com>
2015-07-22 18:36:14 +0100

    solution with system.in/out test

--- src/test/java/org/codetaming/hackerrank/ut/SolutionTest.java
+++ src/test/java/org/codetaming/hackerrank/ut/SolutionTest.java
@@ -3,6 +3,10 @@ package org.codetaming.hackerrank.ut;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -28,5 +32,15 @@ public class SolutionTest {
     @Test
     public void given_starting_height_of_1_and_4_cycles_return_7() throws Exception {
         assertThat(solution.calculateHeight(1, 4), is(7));
+    }
+
+    @Test
+    public void given_test_input_output_correct_output() throws Exception {
+        String data = "3\n0\n1\n4";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Solution.main(new String[]{});
+        assertThat(out.toString(), is("1\n2\n7\n"));
     }
 }
