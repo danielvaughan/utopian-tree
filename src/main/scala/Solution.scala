7c2472183317a99c2f3d62ef36f9be9e62ead60b
Daniel Vaughan <dan@danielvaughan.com>
2015-07-25 09:15:29 +0100

    added passing Scala solution but could be much more elegant

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -1,12 +1,25 @@
+import java.util.Scanner
+
 object Solution {
 
   def main(args: Array[String]) {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
-*/
+    val s: Scanner = new Scanner(System.in)
+    val startingHeight: Int = 1
+    val numTestCases: Int = s.nextInt
+    var testCase: Int = numTestCases
+    while (({
+      testCase -= 1; testCase + 1
+    }) > 0) {
+      val cycles: Int = s.nextInt
+      System.out.println(Solution.calculateHeight(startingHeight, cycles))
+    }
   }
 
-  def calculateHeight(height: Int, cycles: Int): Int =
-  {
-    height
+  def calculateHeight(height: Int, cycles: Int): Int = {
+    def calculateCycleHeight(height: Int, cycle: Int, cycles: Int): Int = {
+      if (cycle > cycles) height
+      else calculateCycleHeight(if (((cycle % 2) == 0)) (height + 1) else (height * 2), cycle+1, cycles)
+    }
+    calculateCycleHeight(height, 1, cycles)
   }
 }
--- src/test/scala/SolutionSpec.scala
+++ src/test/scala/SolutionSpec.scala
@@ -6,4 +6,11 @@ class SolutionSpec extends FlatSpec {
     assert(Solution.calculateHeight(1,0)==1);
   }
 
+  "Solution" should "return 2 for initial height of 1 and 1 cycle" in {
+    assert(Solution.calculateHeight(1,1)==2);
+  }
+
+  "Solution" should "return 7 for initial height of 1 and 4 cycle" in {
+    assert(Solution.calculateHeight(1,4)==7);
+  }
 }
